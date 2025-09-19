6abdb2d6fa6f586c70cdd44ca87048e267b7ac6e
ShanthaKumar01 <144992958+ShanthaKumar01@users.noreply.github.com>
2025-09-20 04:06:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+const navbarToggle=document.querySelector('.navbar-toggle');
+const navbarMenu=document.querySelector('.navbar-menu');
+navbarToggle.addEventListener('click', () => 
+{
+    navbarToggle.classList.toggle('active');
+    navbarMenu.classList.toggle('active');
+}
+);
+
+
+
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach(faq => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("active");
+  })
+})
